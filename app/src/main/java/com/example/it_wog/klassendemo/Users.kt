c2838b08ce7a580eb865b55a52dc5c229a3faf4e
Wolfgang <wolfgang.eberding@googlemail.com>
2018-10-17 15:09:52 +0200

    constructur introduced

--- app/src/main/java/com/example/it_wog/klassendemo/Users.kt
+++ app/src/main/java/com/example/it_wog/klassendemo/Users.kt
@@ -1,9 +1,16 @@
 package com.example.it_wog.klassendemo
 
+import android.util.Log
+
+//primärer constructor
 class Users (name : String, nickname: String, alter : Int){
     private val nameNeu = name
     private val nicknameNeu = nickname
     private val alterNeu = alter
+
+    constructor(name2Constructor: String) : this (name2Constructor, "", 0){
+        Log.i("Test", "2.construtor ausgeführt")
+    }
 
     fun gibName () :String {
         return "Der User heißt $nicknameNeu, sein echter Name ist $nameNeu und sein Alter ist ${gibAlter()}"
