ae167a91adcb26a59802f8e4cd06e346cdf4daa3
Wolfgang <wolfgang.eberding@googlemail.com>
2018-10-17 11:35:03 +0200

    private values and function added

--- app/src/main/java/com/example/it_wog/klassendemo/Users.kt
+++ app/src/main/java/com/example/it_wog/klassendemo/Users.kt
@@ -1,11 +1,16 @@
 package com.example.it_wog.klassendemo
 
 class Users (name : String, nickname: String, alter : Int){
-    val nameNeu = name
-    val nicknameNeu = nickname
-    val alterNeu = alter
+    private val nameNeu = name
+    private val nicknameNeu = nickname
+    private val alterNeu = alter
 
     fun gibName () :String {
-        return "Der User heißt $nicknameNeu, sein echter Name ist $nameNeu"
+        return "Der User heißt $nicknameNeu, sein echter Name ist $nameNeu und sein Alter ist ${gibAlter()}"
+    }
+    //private functions are only in the same class available
+    private fun gibAlter () : Int {
+        return alterNeu
+
     }
 }
